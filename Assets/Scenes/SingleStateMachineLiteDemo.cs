16e566c2952ac18def42dfadaa6d2194cdc33aa6
Fatir Ahmad <djfdat@gmail.com>
2015-11-14 10:28:53 -0800

    First pass on SingleStateMachineLite, serialization not the best

---
+++ Assets/Scenes/SingleStateMachineLiteDemo.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum EDemoStates
+{
+	State1,
+	State2,
+	State3
+}
+
+public class SingleStateMachineLiteDemo : MonoBehaviour, ISerializationCallbackReceiver
+{
+	[SerializeField]
+	private SingleStateMachineLite<EDemoStates> Demo_SSML;
+
+	void Start()
+	{
+		Debug.Log("Starting Demo"); 
+
+		Demo_SSML = new SingleStateMachineLite<EDemoStates>();
+
+		Debug.Log("Subscribing to Events");
+
+		Demo_SSML.OnStateEnter += Demo_SSML_OnStateEnter;
+		Demo_SSML.OnStateExit += Demo_SSML_OnStateExit;
+
+		// Uncomment the line below to allow events to refire when setting activeState to the same value
+		//Demo_SSML.bAllowStateReenter = true;
+	}
+
+	[SerializeField]
+	private EDemoStates __state;
+
+	public void OnBeforeSerialize() 
+	{
+		__state = Demo_SSML.ActiveState;
+	}
+
+	public void OnAfterDeserialize()
+	{
+		Demo_SSML = new SingleStateMachineLite<EDemoStates>();
+		Demo_SSML.OnStateEnter += Demo_SSML_OnStateEnter;
+		Demo_SSML.OnStateExit += Demo_SSML_OnStateExit;
+
+		Demo_SSML.ActiveState = __state;
+
+		Debug.Log(Demo_SSML.ActiveState.ToString());
+	}
+
+	#region Event Handlers
+	private void Demo_SSML_OnStateEnter(EDemoStates state)
+	{
+		Debug.Log("Entering state " + state.ToString());
+
+		switch (state)
+		{
+			case EDemoStates.State1:
+				break;
+			case EDemoStates.State2:
+				break;
+			case EDemoStates.State3:
+				break;
+			default:
+				break;
+		}
+	}
+
+	private void Demo_SSML_OnStateExit(EDemoStates state)
+	{
+		Debug.Log("Exiting state " + state.ToString());
+
+		switch (state)
+		{
+			case EDemoStates.State1:
+				break;
+			case EDemoStates.State2:
+				break;
+			case EDemoStates.State3:
+				break;
+			default:
+				break;
+		}
+	}
+	#endregion Event Handlers
+
+	#region Button Events
+	public void OnClient_SetState1()
+	{
+		Debug.Log("Setting active state to State1");
+		Demo_SSML.ActiveState = EDemoStates.State1;
+	}
+
+	public void OnClient_SetState2()
+	{
+		Debug.Log("Setting active state to State2");
+		Demo_SSML.ActiveState = EDemoStates.State2;
+	}
+
+	public void OnClient_SetState3()
+	{
+		Debug.Log("Setting active state to State3");
+		Demo_SSML.ActiveState = EDemoStates.State3;
+	}
+	#endregion Button Events
+}
---
+++ Assets/Scripts/Extensions/SimpleStateMachine/SingleStateMachineLite.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class SingleStateMachineLite<TEnum>
+{
+	public bool bAllowStateReenter = false;
+
+	[SerializeField]
+	private TEnum activeState;
+	public TEnum ActiveState
+	{
+		get
+		{
+			return activeState;
+		}
+		set
+		{
+			if (IsStateActive(value) && !bAllowStateReenter)
+			{
+				Debug.LogWarning("Attempting to set state that is already active.\nIf this is desired, please set bAllowStateReenter to true on this SingleStateMachineLite instance.");
+				return;
+			}
+
+			if (OnStateExit != null && activeState != null) OnStateExit(activeState);
+			activeState = value;
+			if (OnStateEnter != null && activeState != null) OnStateEnter(activeState);
+		}
+	}
+
+	// For consistency with MultiStateMachineLite;
+	public bool IsStateActive(TEnum state)
+	{
+		return activeState.Equals(state);
+	}
+
+	#region Delegates
+	public delegate void OnStateEnterDelegate(TEnum state);
+	public delegate void OnStateExitDelegate(TEnum state);
+	#endregion Delegates
+
+	#region Events
+	public event OnStateEnterDelegate OnStateEnter;
+	public event OnStateExitDelegate OnStateExit;
+	#endregion Events
+}
